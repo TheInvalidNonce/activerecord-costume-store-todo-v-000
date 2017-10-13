4d6aeb1b459684c71ddd2b4a65e2ea6232e39793
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-13 20:18:59 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :opening_hours
-      t.datetime :closing_hours
+      t.datetime :opening_time
+      t.datetime :closing_Time
       t.timestamps
     end
   end
