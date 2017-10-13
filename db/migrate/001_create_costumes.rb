f5e5de870923ed1cf49b687d362a8284c4939934
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-13 20:12:00 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,7 +7,7 @@ class CreateCostumes < ActiveRecord::Migration
       t.string :image_url
       t.timestamps
     end
-    end
+  end
 end
 
 # By convention, the class name should match the part of the
